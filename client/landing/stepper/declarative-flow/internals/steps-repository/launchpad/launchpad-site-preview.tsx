0422fc4d0723b52a2ad44dcdf6db719c86f29df0
Sharpirate <stoynov.bogomil@gmail.com>
2023-01-13 11:40:38 -0700

    rename prop & add aria-label

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/launchpad-site-preview.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/launchpad-site-preview.tsx
@@ -106,7 +106,7 @@ const LaunchpadSitePreview = ( {
 				defaultViewportDevice={ defaultDevice }
 				devicesToShow={ devicesToShow }
 				showSiteAddressBar={ false }
-				isLaunchpad
+				enableEditOverlay
 			/>
 		</div>
 	);
