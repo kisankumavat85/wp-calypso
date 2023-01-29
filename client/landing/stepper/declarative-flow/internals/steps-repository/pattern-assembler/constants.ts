334214e970b6471f174f80dfc0b6c6750a46306e
Miguel <miguel.san.segundo@automattic.com>
2023-01-29 21:06:29 +0700

    Use posts from dotcomverticalposts

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/constants.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/constants.ts
@@ -2,3 +2,5 @@ export const PATTERN_SOURCE_SITE_ID = 174455321; // dotcompatterns
 export const PUBLIC_API_URL = 'https://public-api.wordpress.com';
 export const PREVIEW_PATTERN_URL = PUBLIC_API_URL + '/wpcom/v2/block-previews/pattern';
 export const SITE_TAGLINE = 'Site Tagline';
+export const BLANK_CANVAS_DEMO_SITE_ID = 211865921;
+export const POST_SITE_ID = 207519425; // dotcomverticalposts.wordpress.com
